b1045c44829de384e9c98dd91fd3e342c1f26f0a
CesarCondori-cmd <cesinhardaniel@gmail.com>
2025-09-09 08:16:49 -0400

    cambios

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\SaludoCesarController;
+use App\Http\Controllers\SaludoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', function () {
+    return view('welcome');
+});
+
+Route::get('/saludo', [SaludoController::class, 'index']);
+Route::get('/saludo_cesar', [SaludoCesarController::class, 'index']);
